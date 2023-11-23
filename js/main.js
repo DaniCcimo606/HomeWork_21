9b00a59e0083e85eda903f0cd35d98099741e196
DaniCcimo <bessmertnyj228@gmail.com>
2023-11-23 02:11:27 +0200

    Finished homework_21

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+// 1
+
+// function toDegree(num, degree) {
+//     return num ** degree;
+// }
+
+// console.log(toDegree(4, 2));
+
+// 2
+
+// let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// function removeElement(arr, item) {
+//     arr.splice(item - 1, 1);
+//     return console.log(arr);
+// }
+
+// console.log(removeElement(myArr, 8));
+
+// 3
+
+// let thisKey = 'abcdefghijklmnopqrstuvwxyz0123456789';
+// let key = '';
+
+// function generateKey(keySymbol, length) {
+//     for (let i = 0; i < length; i++) {
+//         let randomIndex = Math.floor(Math.random() * keySymbol.length)
+//         key += thisKey.charAt(randomIndex);
+//     }
+//     return key;
+// }
+
+// console.log(generateKey(thisKey, 16));
